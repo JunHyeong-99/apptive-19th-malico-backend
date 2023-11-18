b0a4171896165da2751070a439872b6400a5257f
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-11-19 03:00:35 +0900

    Refactor User Auth Controller with Enhanced Email Verification
    UserAuthController의 이메일 인증 및 사용자 찾기 로직을 개선했습니다. 중복된 @GetMapping 메서드를 주석 처리하고, RESTful 설계를 강화하기 위해 URL 구조와 메서드 구현을 재정비했습니다.

--- src/main/java/com/apptive/marico/controller/UserAuthController.java
+++ src/main/java/com/apptive/marico/controller/UserAuthController.java
@@ -1,20 +1,10 @@
 package com.apptive.marico.controller;
 
 
-<<<<<<< Updated upstream:src/main/java/com/apptive/marico/controller/UserAuthController.java
 import com.apptive.marico.dto.findId.UserFindIdResponseDto;
 import com.apptive.marico.dto.findPwd.ChangePwdResponseDto;
 import com.apptive.marico.dto.findPwd.NewPwdRequestDto;
 import com.apptive.marico.dto.findId.SendEmailRequestDto;
-=======
-import com.apptive.marico.dto.LoginDto;
-import com.apptive.marico.dto.findId.SendEmailRequestDto;
-import com.apptive.marico.dto.findId.UserFindIdResponseDto;
-import com.apptive.marico.dto.findPwd.ChangePwdResponseDto;
-import com.apptive.marico.dto.findPwd.NewPwdRequestDto;
-import com.apptive.marico.dto.token.TokenRequestDto;
-import com.apptive.marico.dto.token.TokenResponseDto;
->>>>>>> Stashed changes:src/main/java/com/apptive/marico/controller/auth/UserAuthController.java
 import com.apptive.marico.dto.verificationToken.SendEmailResponseDto;
 import com.apptive.marico.dto.verificationToken.VerificationTokenResponseDto;
 import com.apptive.marico.entity.Member;
@@ -25,6 +15,7 @@ import com.apptive.marico.repository.StylistRepository;
 import com.apptive.marico.service.MemberAuthService;
 import com.apptive.marico.service.StylistAuthService;
 import com.apptive.marico.service.VerificationTokenService;
+import com.apptive.marico.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,6 +36,7 @@ public class UserAuthController {
     private final MemberAuthService memberAuthService;
 
 
+    //TODO: 회원가입시 이메일 인증을 받는 로직이라면 추가적인 로직의 변형이 필요합니다.
     @PostMapping("/sign/verification-code")
     public ResponseEntity<SendEmailResponseDto> sendEmailForSign(@RequestBody SendEmailRequestDto sendEmailRequestDto) {
         String email = sendEmailRequestDto.getEmail();
@@ -54,19 +46,7 @@ public class UserAuthController {
     public ResponseEntity<VerificationTokenResponseDto> checkCodeForSign(@RequestParam String code) {
         return ResponseEntity.ok(new VerificationTokenResponseDto(verificationTokenService.verifyUserEmailForSign(code)));
     }
-<<<<<<< Updated upstream:src/main/java/com/apptive/marico/controller/UserAuthController.java
-=======
 
-    @PostMapping("/sign/verification-code")
-    public ResponseEntity<SendEmailResponseDto> sendEmailForSign(@RequestBody SendEmailRequestDto sendEmailRequestDto) {
-        String email = sendEmailRequestDto.getEmail();
-        return ResponseEntity.ok(new SendEmailResponseDto(verificationTokenService.createVerificationTokenForSign(email)));
-    }
-    @GetMapping("/sign/verification-code")
-    public ResponseEntity<VerificationTokenResponseDto> checkCodeForSign(@RequestParam String code) {
-        return ResponseEntity.ok(new VerificationTokenResponseDto(verificationTokenService.verifyUserEmailForSign(code)));
-    }
->>>>>>> Stashed changes:src/main/java/com/apptive/marico/controller/auth/UserAuthController.java
     @PostMapping("/search/verification-code")
     public ResponseEntity<SendEmailResponseDto> sendEmailForFind(@RequestBody SendEmailRequestDto sendEmailRequestDto) {
         String email = sendEmailRequestDto.getEmail();
@@ -77,26 +57,25 @@ public class UserAuthController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new SendEmailResponseDto( "오류가 발생했습니다: " + e.getMessage()));
         }
     }
-    @GetMapping("/search/verification-code")
-    public ResponseEntity<VerificationTokenResponseDto> checkCodeForIdOrPwd(@RequestParam String code) {
-        return ResponseEntity.ok(new VerificationTokenResponseDto(verificationTokenService.verifyUserEmailForIdOrPwd(code)));
-    }
+//    @GetMapping("/search/verification-code") //구현의 이유가 무엇인가요?
+//    public ResponseEntity<VerificationTokenResponseDto> checkCodeForIdOrPwd(@RequestParam String code) {
+//        return ResponseEntity.ok(new VerificationTokenResponseDto(verificationTokenService.verifyUserEmailForIdOrPwd(code)));
+//    }
 
     @GetMapping("/search/id")
-    public ResponseEntity<UserFindIdResponseDto> returnId(@RequestParam String code) {
-        return ResponseEntity.ok(verificationTokenService.returnUserId(code));
+    public ResponseEntity<?> returnId(@RequestParam String code) {
+        return ResponseEntity.ok(ApiUtils.success(verificationTokenService.returnUserEmail(code)));
     }
 
-    @PatchMapping("/search/password")
-    public ResponseEntity<ChangePwdResponseDto> checkCodeForPwd(@RequestBody NewPwdRequestDto newPwdRequestDto) throws Exception {
-        Optional<Stylist> findStylist = stylistRepository.findByUserId(newPwdRequestDto.getUserId());
-        Optional<Member> findMember = memberRepository.findByUserId(newPwdRequestDto.getUserId());
-        if(findStylist.isPresent()) {
-            return ResponseEntity.ok(new ChangePwdResponseDto(stylistAuthService.changePassword(findStylist.get(), newPwdRequestDto.getPassword(), newPwdRequestDto.getCode())));
+    @PatchMapping("/search/password") //Restful 한 Url 설계가 필요해보입니다!
+    public ResponseEntity<?> checkCodeForPwd(@RequestBody NewPwdRequestDto newPwdRequestDto) throws Exception {
+        Optional<Stylist> stylist = stylistRepository.findByEmail(newPwdRequestDto.getUserEmail());
+        Optional<Member> member = memberRepository.findByEmail(newPwdRequestDto.getUserEmail());
+        if(stylist.isPresent()) {
+            return ResponseEntity.ok(ApiUtils.success(stylistAuthService.changePassword(stylist.get() ,newPwdRequestDto)));
         }
-
-        else if (findMember.isPresent()) {
-            return ResponseEntity.ok(new ChangePwdResponseDto(memberAuthService.changePassword(findMember.get(), newPwdRequestDto.getPassword(), newPwdRequestDto.getCode())));
+        else if (member.isPresent()) {
+            return ResponseEntity.ok(new ChangePwdResponseDto(memberAuthService.changePassword(member.get(), newPwdRequestDto.getPassword(), newPwdRequestDto.getCode())));
         }
         else {
             throw new CustomException(USER_NOT_FOUND);
