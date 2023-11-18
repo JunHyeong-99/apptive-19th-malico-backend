f2adc6f0e16bbf9a8c0f8583340bb029491366fc
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-11-19 03:01:59 +0900

    Enhance Error Handling in Authentication Process
    인증 과정의 에러 처리를 개선했습니다. 새로운 오류 코드 VERIFICATION_CODE_INVALID와 EMAIL_DOES_NOT_MATCH를 추가하여 사용자 인증 시 발생할 수 있는 에러 상황을 더 명확하게 구분하고 처리합니다. 또한, 기존의 VERIFICATION_CODE_TIMEOUT 메시지를 업데이트하여 사용자에게 보다 정확한 정보를 제공합니다.

--- src/main/java/com/apptive/marico/exception/ErrorCode.java
+++ src/main/java/com/apptive/marico/exception/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
     PASSWORD_NOT_MATCH(400, "잘못된 비밀번호입니다."),
     CODE_NOT_MATCH(400, "인증 번호가 잘못되었습니다."),
 
+    VERIFICATION_CODE_INVAILD(401, "발급 코드가 유효하지 않습니다."),
+    EMAIL_DOES_NOT_MATCH(401, "이메일이 일치하지 않습니다."),
+
     //404 NOT_FOUND 잘못된 리소스 접근
     EMAIL_NOT_FOUND(404, "존재하지 않는 이메일입니다."),
     USER_NOT_FOUND(404, "존재하지 않는 회원입니다."),
@@ -18,7 +21,7 @@ public enum ErrorCode {
     ALREADY_SAVED_EMAIL(409, "이미 저장된 이메일입니다."),
 
     //419 Authentication Timeout
-    VERIFICATION_CODE_TIMEOUT(419, "인증 시간이 초과 되었습니다."),
+    VERIFICATION_CODE_TIMEOUT(419, "발급 코드의 인증 시간이 초과 되었습니다."),
 
     //500 INTERNAL SERVER ERROR
     INTERNAL_SERVER_ERROR(500, "서버 에러입니다. 서버 팀에 연락주세요!"),
