0f71abdde9f05ae3fcb9e3390a1c3ccf95c0ed7a
이지은 <jieun7056@naver.com>
2023-11-26 13:16:59 +0900

    feat: member 마이페이지의 비밀번호 변경 기능 구현 비밀번호가 일치한지 검사하는 api, 비밀번호 변경 api

--- src/main/java/com/apptive/marico/controller/MemberMypageController.java
+++ src/main/java/com/apptive/marico/controller/MemberMypageController.java
@@ -1,14 +1,23 @@
 package com.apptive.marico.controller;
 
+import com.apptive.marico.dto.findPwd.NewPwdRequestDto;
 import com.apptive.marico.dto.member.MemberInformationDto;
 import com.apptive.marico.dto.member.MemberMypageDto;
 import com.apptive.marico.dto.mypage.LikedStylistListDto;
+import com.apptive.marico.entity.Member;
+import com.apptive.marico.entity.Stylist;
+import com.apptive.marico.exception.CustomException;
 import com.apptive.marico.service.MemberMypageService;
+import com.apptive.marico.utils.ApiUtils;
+import com.apptive.marico.utils.ApiUtils.ApiSuccess;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Optional;
+
+import static com.apptive.marico.exception.ErrorCode.USER_NOT_FOUND;
 
 @RestController
 @RequestMapping("/api/mypage/member")
@@ -37,6 +46,18 @@ public class MemberMypageController {
     @GetMapping("/information")
     public ResponseEntity<MemberInformationDto> findInformation(Principal principal) {
         return ResponseEntity.ok(memberMyPageService.findInformation(principal.getName()));
+    }
+
+    // 현재 비밀번호가 일치한지 검사
+    @GetMapping("/password")
+    public ResponseEntity<ApiSuccess<Object>> checkCurrentPassword(Principal principal, @RequestParam String currentPassword) {
+        return ResponseEntity.ok(new ApiSuccess<>(memberMyPageService.CheckCurrentPassword(principal.getName(), currentPassword)));
+    }
+    // 비밀번호 변경
+    @PatchMapping("/password")
+    public ResponseEntity<?> changePassword(Principal principal, @RequestParam String newPassword) {
+        return ResponseEntity.ok(new ApiUtils.ApiSuccess<>(memberMyPageService.changePassword(principal.getName(), newPassword)));
+
     }
 
 }
